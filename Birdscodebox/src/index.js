bfb46186799093983e79bc471e99a28da0ba8384
Elegant <103381086+Elegangit@users.noreply.github.com>
2022-11-04 20:22:27 -0400

    Add files via upload

---
+++ Birdscodebox/src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  // body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
